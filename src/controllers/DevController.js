431b9ff4992c5135565c6b93ad15bf498f6c58a0
frfontoura <frfontoura@gmail.com>
2020-01-17 17:07:57 -0300

    SocketIo configuration

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
+
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
   async index(req, res) {
@@ -31,6 +33,14 @@ module.exports = {
         techs: techsArray,
         location,
       });
+
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      )
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
+
     }
 
     return res.json(dev);
--- src/utils/parseStringAsArray.js
+++ src/utils/parseStringAsArray.js
@@ -1,3 +1,8 @@
 module.exports = function parseStringAsArray(arrayAsString) {
+
+  if(!arrayAsString) {
+    return []
+  }
+
   return arrayAsString.split(',').map(s => s.trim());
 }
